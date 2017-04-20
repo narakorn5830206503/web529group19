4da017cf4e78f09d8175fe31c4d1f2a3534d54b5
narakorn5830206503 <boombim.kku@gmail.com>
2017-04-20 16:55:02 +0700

    edit

--- main.php
+++ main.php
@@ -1,2 +1,11 @@
-<?php
-    echo 'Hello world!1';
+<html>
+  <head>
+    <meta charset="utf-8"/>
+	<title> โครงงานกลุ่ม 19 </title>
+  </head>
+
+<body>
+<h1>โครงงานกลุ่ม</h1>
+ทดสอบแก้ไขไฟล์
+</body>
+<html>
