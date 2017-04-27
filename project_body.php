ef0c829d9004dd31c5d50b5a9a8b0390a657d094
onpree <onpreeyasomp@gmail.com>
2017-04-27 15:19:09 +0700

    UPDATE

---
+++ project_body.php
@@ -0,0 +1,16 @@
+<?php
+use google\appengine\api\users\UserService;
+global $appid,$page,$title;
+$phpfile="project_body_$page.php";
+if(file_exists($phpfile)){
+include($phpfile);
+}else{
+$htmlfile = "gs://$appid/$page.html";
+if(file_exists($htmlfile)){
+readfile($htmlfile);
+}
+if (UserService::isCurrentUserAdmin()){
+echo "<br><a href='project.php?p=edit&file=$page.html'";
+}
+}
+?>
